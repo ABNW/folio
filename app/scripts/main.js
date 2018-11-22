48aa218ea90668843daf1d68ec111fb6e0473da2
Miles Tincknell <miles.tincknell@gmail.com>
2018-11-22 21:22:56 +0000

    grabbing triangles programmatically

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,42 +1,3 @@
-document.onreadystatechange = function () {
-  if (document.readyState == "interactive") {
-    let triangles0 = document.getElementsByClassName('st0');
-    let triangles1 = document.getElementsByClassName('st1');
-    let triangles2 = document.getElementsByClassName('st2');
-    let triangles3 = document.getElementsByClassName('st3');
-    let triangles4 = document.getElementsByClassName('st4');
-
-    let set = [triangles0, triangles1, triangles2, triangles3, triangles4];
-    let dampeners = [0.01, 0.015, 0.01, 0.02, 0.01];
-
-    const smoothMove$ = animationFrame$
-      .withLatestFrom(move$, (tick, move) => move)
-      .scan(lerp);
-
-    smoothMove$.subscribe(pos => {
-
-      for (let [index, value] of set.entries()) {
-        for (let triangle of value) {
-          // let x = pos.x * (index % 2 ? dampeners[index] : -dampeners[index]);
-          // let y = pos.y * (index % 2 ? dampeners[index] : -dampeners[index]);
-          let x = pos.x * (index == 4 ? dampeners[index] : -dampeners[index]);
-          let y = pos.y * (index == 4 ? dampeners[index] : -dampeners[index]);
-          triangle.setAttribute('transform', `translate(${x}, ${y})`);
-        }
-      }
-    });
-  }
-}
-
-// function moveTriangles(e, set, dampener) {
-//     let dampen = dampener || 0.01;
-//     let x = e.clientX * dampen;
-//     let y = e.clientY * dampen;
-//     for(var thing of set) {
-//         thing.setAttribute('transform', `translate(${x}, ${y})`);
-//     }
-// }
-
 const docElm = document.documentElement;
 const {
   clientWidth,
@@ -78,15 +39,46 @@ function lerp(start, end) {
   };
 }
 
-// Mouse/touch moves linearly interpolated
-// on every animation frame
+const classNames = ['st0', 'st1', 'st2', 'st3', 'st4'];
+const ids = ['project-left-1', 'project-right-1', 'project-left-2', 'project-right-2'];
+let sets = [[]];
+const dampeners = [0.01, 0.015, 0.01, 0.02, 0.01];   
 
-// Apply values to styles
-// smoothMove$.subscribe(pos => {
-//     const rotX = (pos.y / clientHeight * -50) + 25;
-//     const rotY = (pos.x / clientWidth * 50) - 25;
+document.onreadystatechange = function () {
+  
+  if (document.readyState == 'interactive') { 
+    
+    let count = 0;
+    
+    for (let id of ids){
+      for (let className of classNames) {
+        if(sets[count] !== undefined ) {
+          sets[count].push(document.getElementById(id).getElementsByClassName(className));
+        } else {
+          sets.push([]);
+          sets[count].push(document.getElementById(id).getElementsByClassName(className))
+        }
+      }
+      count++;
+    }
 
-//     cardElm.style.cssText = `
-//         transform: rotateX(${rotX}deg) rotateY(${rotY}deg);
-//     `;
-// });
+    const smoothMove$ = animationFrame$
+      .withLatestFrom(move$, (tick, move) => move)
+      .scan(lerp);
+
+    smoothMove$.subscribe(pos => {
+
+      for (let set of sets) {
+        for (let [index, value] of set.entries() ) {
+          for (let triangle of value) {
+            let x = pos.x * (index == 4 ? -dampeners[index] : dampeners[index]);
+            let y = pos.y * (index == 4 ? -dampeners[index] : dampeners[index]);
+            const rotX = (pos.y / clientHeight * -50) + 25;
+            const rotY = (pos.x / clientWidth * 50) - 25;
+            triangle.setAttribute('transform', `translate(${x}, ${y})`);
+          }
+        } 
+      }
+    });
+  }
+}
